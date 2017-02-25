b861b1e88120f38b8a3f8f5efee41083f0fbd9a4
Jacek Poczatek <jac.poczatek@gmail.com>
2017-02-25 15:08:20 +0100

    Tutorial_3

--- src/main/java/tutorial/Calculator.java
+++
@@ -1,8 +0,0 @@
-package tutorial;
-
-public class Calculator {
-    public int abs(int arg){
-        if (arg < 0) return arg;
-        return arg;
-    }
-}
--- src/main/java/tutorial/Engine.java
+++
@@ -1,6 +0,0 @@
-package tutorial;
-
-public interface Engine {
-    void increaseRpm();
-    int getRpm();
-}
